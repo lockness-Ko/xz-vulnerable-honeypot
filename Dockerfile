cb6269931c26dd89afd28ce2e1ef6bd8224bde73
lockness-Ko <videolk9373@gmail.com>
2024-03-31 22:28:06 +1100

    (update) modify bpftrace, add backup strace hook
    TODO: add gdb hook into sshd

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM ghcr.io/lockness-ko/xz-backdoor:latest
 LABEL org.opencontainers.image.source=https://github.com/lockness-Ko/xz-vulnerable-honeypot
 
+RUN [ "pacman", "-Sy", "--noconfirm", "--needed", "tcpdump", "bpftrace" ]
+
 COPY ./run.sh /run.sh
 
 ENTRYPOINT [ "/run.sh" ]
--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-time env -i LANG=C /usr/sbin/sshd -E /var/log/sshd.log
-/pspy64 --ppid | tee -a /var/log/sshd.log &
+chown root:root /usr/share/empty.sshd
+time env -i LANG=C /usr/sbin/sshd -E /logs/sshd_$(date +%d%m%y_%H%M).log
+tcpdump -i any -w /logs/tshark_$(date +%d%m%y_%H%M).pcap &
+# bpftrace -e "tracepoint:syscalls:sys_enter_execve { if (curtask->parent->pid == $(ps aux | grep sshd | awk '{print $2}' | head -n1 | tr -d '\n')) { printf(\"%d \", tid); join(args->argv); } }" | tee /logs/bpftrace_$(date +%d%m%y_%H%M).log
+bpftrace -e "tracepoint:syscalls:sys_enter_execve { if (curtask->parent->comm == \"sshd\") { printf(\"%d \", tid); join(args->argv); } }" | tee /logs/bpftrace_$(date +%d%m%y_%H%M).log
+strace -e execve -p $(ps aux | grep sshd | awk '{print $2}' | head -n1 | tr -d '\n') | tee /logs/strace_$(date +%d%m%y_%H%M).log
 
-tail -f /var/log/sshd.log
+tail -f /logs/*$(date +%d%m%y_%H%M).log
